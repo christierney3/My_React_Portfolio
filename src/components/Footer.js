27113c904300fd7ca28ee4771e97b08e91046f4d
christierney3 <chris.tierney3@gmail.com>
2022-11-15 11:01:35 -0700

    updated resume

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
         <ul class="contact-links">
             <li class="link"><a href="https://www.linkedin.com/in/chris-tierney-833611180/">LinkedIn</a></li>
             <li class="link"><a href="https://github.com/christierney3">GitHub</a></li>
-            <li class="link"><a href={`${process.env.PUBLIC_URL}/images/dev_resume.pdf`} download='ct_resume'>Resume</a></li>
+            <li class="link"><a href={`${process.env.PUBLIC_URL}/images/CT_Apex_Resume.pdf`} download='ct_resume'>Resume</a></li>
         </ul>
     </footer>
     )
